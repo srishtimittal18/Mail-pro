5eabe00e52ff13d42c4c98357d78df8ed5b53bf1
rishabhtyagi2306 <rishabhtyagi.2306@gmail.com>
2019-09-07 20:09:40 +0530

    Started Main Project

---
+++ MailPro/Models/Membership.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MailPro.Models
+{
+    public class Membership
+    {
+        public int FacultyID { get; set; }
+        public string FacultyName { get; set; }
+        public string FacultyEmail { get; set; }
+        public int FacultyPhoneNo { get; set; }
+        public string Department { get; set; }
+        public string Password { get; set; }
+        public virtual ICollection<CategoryTable> CategoryTable { get; set; }
+    }
+}
